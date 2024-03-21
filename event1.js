8fb69dd8e9ea3f224ebf58de4f861339d46315d1
Alishxubb <alishbaalish889@gmail.com>
2024-03-21 23:46:23 +0500

    update

---
+++ event1.js
@@ -0,0 +1,16 @@
+function update(){
+    console.log("event is fired");
+}
+//i want to create variable
+var x = 10;
+var y = 20;
+console.log(x*y);
+// i want to create function in js
+function sum (a,b){
+return a+b;
+}
+
+console.log(sum(x,y));
+//if i want to assign function to variable
+var mysum = sum;
+console.log(mysum);
